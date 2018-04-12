410a0f75ca448966dd8bae661158b30b0399a42b
Admin <admin@Admins-MacBook-Pro.local>
2018-04-12 13:32:07 +0100

    Added images and videos

--- js/main.js
+++ js/main.js
@@ -3,48 +3,116 @@ function removeClass(ele,cls) {
    ele.className = ele.className.replace(reg,' ');
 }
 
+function hasClass(ele, cls) {
+	return new RegExp('(^| )' + cls + '( |$)', 'gi').test(ele.className);
+}
+
 var Karim = (function () {
 
-	var init = function () {
-		console.log('Running Karim!');
-		initWaypoints();
-		initImages();
-	};
+	return {
+		switchSrc: function(el) {
+			var src = el.getAttribute('data-src');
+			el.setAttribute('src', src);
+		},
 
-	var initImages = function () {
-		var hero = document.querySelector('#hero');
-		if(hero) {
+		initImages: function() {
+			var hero = document.querySelector('#hero');
+
+			//load images async
+			var images = document.querySelectorAll('.asyncImage');
+			var i = 0;
+			var length = images.length;
+			for(i; i < length; i++) {
+				var image = images[i];
+				this.switchSrc(image);
+			}
+
+			if(!hero) {  return; }
+
 			imagesLoaded(hero, function () {
 				hero.className += ' ' + 'imagesLoaded';
 			});
+		},
+
+		initVideos: function() {
+			var videos = document.querySelectorAll('.asyncVideo');
+			var i = 0;
+			var length = videos.length;
+
+			var stopAllVideos = function () {
+				var x = 0;
+				for(x; x < length; x++) {
+					videos[x].currentTime = 0;
+					videos[x].pause();
+				}
+			}
+
+			var createWaypoint = function (video, container) {
+				new Waypoint({
+					element: container,
+					handler: function (direction) {
+						console.log(video);
+						if(direction === 'up') {
+							stopAllVideos();
+						} else {
+							stopAllVideos();
+
+							video.play();
+						}
+					},
+					offset: '20%'
+				});
+			};
+
+			//each video add handler and init with src change
+			for(i; i < length; i++) {
+				var video = videos[i];
+				this.switchSrc(video);
+
+
+				var container = video.parentNode.parentNode.parentNode;
+				// video.addEventListener('mouseover', function() {
+				// 	//LOOP over all containers and remove class
+				// 	var portfolioItems = document.querySelectorAll('.portfolio__item');
+				// 	for(var x = 0; x < portfolioItems.length; x++) {
+				// 		removeClass(portfolioItems[x], 'videoStarted');
+				// 	}
+                //
+				// 	stopAllVideos();
+				// 	container.className += ' ' + 'videoStarted';
+				// 	video.play();
+				// });
+				// video.addEventListener('mouseout', function() {
+				// 	console.log('mouseout');
+				// });
+				createWaypoint(video, container);
+			}
+		},
+
+		initNavigation: function() {
+			var body = document.getElementsByTagName('body').item(0);
+
+			var navChange = function (direction) {
+				if(direction === 'up') {
+					removeClass(body, 'navStick');
+				} else {
+					body.className += ' ' + 'navStick';
+				}
+			};
+
+			var navWaypoint = new Waypoint({
+				element: document.getElementById('portfolio'),
+				handler: navChange,
+				offset: 50
+			});
+		},
+
+		init: function() {
+			this.initNavigation();
+			this.initImages();
+			this.initVideos();
 		}
 	};
-
-	var initWaypoints = function () {
-		var body = document.getElementsByTagName('body').item(0);
-
-		var navChange = function (direction) {
-			if(direction === 'up') {
-				removeClass(body, 'navStick');
-				console.log('Hit up');
-
-			} else {
-				console.log('Hit down');
-				body.className += ' ' + 'navStick';
-			}
-		};
-
-		var navWaypoint = new Waypoint({
-			element: document.getElementById('portfolio'),
-			handler: navChange,
-			offset: 40
-		});
-
-	};
-
-	return {
-		init: init
-	}
 })();
 
 window.onload = function () {
